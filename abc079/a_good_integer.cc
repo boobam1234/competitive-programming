db8f9494d41b4256c0c446bd53dcda6dd373563c
boobam <47242815+boobam1279@users.noreply.github.com>
2019-06-25 23:15:15 +0900

    feat:abc079A問題修正

--- abc079/a_good_integer.cc
+++ abc079/a_good_integer.cc
@@ -2,24 +2,16 @@
 
 int main() {
   // 入力
-  int num;
-  std::cin >> num;
+  std::string n;
+  std::cin >> n;
 
   // 良い整数判定
-  int thousands_place = num / 1000;
-  int hundreds_place = num % 1000 / 100;
-  int tens_place = num % 100 / 10;
-  int ones_place = num % 10;
-  std::string is_good_integer;
-  if ((thousands_place == hundreds_place && hundreds_place == tens_place) || 
-    (hundreds_place == tens_place && tens_place == ones_place)) {
-      is_good_integer = "Yes";
+  if (n[0] == n[1] && n[1] == n[2] ||
+    n[1] == n[2] && n[2] == n[3]) {
+    std::cout << "Yes" << std::endl;
   } else {
-    is_good_integer = "No";
+    std::cout << "No" << std::endl;
   }
-
-  // 出力
-  std::cout << is_good_integer << std::endl;
 
   return 0;
 }
