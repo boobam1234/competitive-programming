368a301613edaa2dd7b3e48fc935d53c44fba4ac
boobam <47242815+boobam1279@users.noreply.github.com>
2019-08-15 19:59:29 +0900

    feat:abc109A問題追加

---
+++ abc109/a_ABC333.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  // 入力
+  int a, b;
+  std::cin >> a >> b;
+
+  // 2の倍数 * c = 偶数
+  if (a * b % 2 == 1) {
+    std::cout << "Yes" << std::endl;
+  } else {
+    std::cout << "No" << std::endl;
+  }
+
+  return 0
+}
