1e68f42093de533906fff50fac2115870a501006
boobam <47242815+boobam1279@users.noreply.github.com>
2020-01-21 22:22:08 +0900

    abc074A問題追加

--- abc074/a_bichrome_cells.cc
+++ abc074/a_bichrome_cells.cc
@@ -1,16 +1,9 @@
 #include <iostream>
-
+ 
 int main() {
-  // 入力
-  int total_squares;
-  int white_num;
-  std::cin >> total_squares >> white_num;
+  int side_length, white_num;
+  std::cin >> side_length >> white_num;
 
-  // 黒色に塗ることになるマスの数
-  int black_num = total_squares * total_squares - white_num;
-
-  // 出力
+  int black_num = side_length * side_length - white_num;
   std::cout << black_num << std::endl;
-
-  return 0;
 }
