176b1193e53d103e584036fd5f5055f39234975a
boobam <47242815+boobam1279@users.noreply.github.com>
2020-01-22 18:43:25 +0900

    abc082A問題修正

--- abc082/a_round_up_the_mean.cc
+++ abc082/a_round_up_the_mean.cc
@@ -1,15 +1,9 @@
 #include <iostream>
 
 int main() {
-  // 入力
   int a, b;
   std::cin >> a >> b;
-
-  // 平均切り上げ
-  int round_up_avg = (a + b + 1) / 2;
-
-  // 出力
-  std::cout << round_up_avg << std::endl;
-
-  return 0;
+  
+  int x = (a + b + 1) / 2;
+  std::cout << x << std::endl;
 }
