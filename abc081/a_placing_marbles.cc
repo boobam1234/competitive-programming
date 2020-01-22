1d23900588813484d4d6b4398b125658f91d3040
boobam <47242815+boobam1279@users.noreply.github.com>
2020-01-22 18:47:49 +0900

    abc081A問題修正

--- abc081/a_placing_marbles.cc
+++ abc081/a_placing_marbles.cc
@@ -1,13 +1,7 @@
 #include <iostream>
 
 int main() {
-  // 入力
-  int s;
-  std::cin >> s;
-
-  // 自然数n ≡ 自然数nの数字和 (mod 9)
-  int ans = s % 9;
-
-  // 出力
-  std::cout << ans << std::endl; 
+  int num;
+  std::cin >> num;
+  std::cout << num % 9 << std::endl;
 }
