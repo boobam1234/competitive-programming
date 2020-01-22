cc7f64399e38bcfdde6929e16c62de40c5f9ab05
boobam <47242815+boobam1279@users.noreply.github.com>
2020-01-22 19:04:27 +0900

    abc088A問題修正

--- abc088/a_infinite_coins.cc
+++ abc088/a_infinite_coins.cc
@@ -1,21 +1,9 @@
 #include <iostream>
 
 int main() {
-  // 入力
-  int payment, one_yen_num;
-  std::cin >> payment >> one_yen_num;
-
-  // 計算
-  int required_one_yen_num = payment % 500;
-  std::string ans;
-  if (one_yen_num >= required_one_yen_num) {
-    ans = "Yes";
-  } else {
-    ans = "No";
-  }
-
-  // 出力
-  std::cout << ans << std::endl;
-
-  return 0;
+  int payment_amount, one_yen_coin_num;
+  std::cin >> payment_amount >> one_yen_coin_num;
+  
+  std::string can_pay =  one_yen_coin_num >= payment_amount % 500 ? "Yes" : "No";
+  std::cout << can_pay << std::endl;
 }
