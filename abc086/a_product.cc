c99d1062631390703d38da4b6ab4975346e4666b
boobam <47242815+boobam1279@users.noreply.github.com>
2020-01-23 18:41:26 +0900

    abc086A問題追加

--- abc086/a_product.cc
+++ abc086/a_product.cc
@@ -1,15 +1,9 @@
 #include <iostream>
 
 int main() {
-  // 入力
   int a, b;
   std::cin >> a >> b;
 
-  // 積の奇遇判定
-  std::string ans = (a * b % 2) ? "Odd" : "Even";
-
-  // 出力
-  std::cout << ans << std::endl;
-
-  return 0;
+  std::string is_odd_or_even = a * b % 2 == 0 ? "Even" : "Odd";
+  std::cout << is_odd_or_even << std::endl;
 }
