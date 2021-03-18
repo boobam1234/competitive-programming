c47f377f5b0b0b2e319b3e7d5c10d9b3006b371d
boobam <47242815+boobam1279@users.noreply.github.com>
2021-03-18 23:37:39 +0900

    feat: Add AOJ ITP1 2_d_circle_in_a_rectangle

--- aoj/itp1/topic2/2_d_circle_in_a_rectangle.cc
+++ aoj/itp1/topic2/2_d_circle_in_a_rectangle.cc
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <algorithm>
 
 int main() {
-  int width, height, x, y, r;
-  std::cin >> width >> height >> x >> y >> r;
-
-  int remaining_length = std::min(std::min(width - x, x), std::min(height - y, y));
-  if (remaining_length >= r) {
+  int w, h, x, y, r;
+  std::cin >> w >> h >> x >> y >> r;
+  if (x - r >= 0 && x + r <= w && y - r >= 0 && y + r <= h) {
     std::cout << "Yes" << std::endl;
   } else {
     std::cout << "No" << std::endl;
   }
 }
+
