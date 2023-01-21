156afae0fc1c3e2f9a76eaca87307e3bd5ca7847
boobam <47242815+boobam1279@users.noreply.github.com>
2023-01-21 14:38:32 +0900

    feat: add abc090 b_palindromic_numbers.cc

---
+++ atcoder/abc/090/b_palindromic_numbers.cc
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+
+bool is_palindrome_num(int num) {
+  std::string num_str = std::to_string(num);
+  std::reverse(num_str.begin(), num_str.end());
+  return num == std::stoi(num_str);
+}
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  int a, b;
+  std::cin >> a >> b;
+  int count = 0;
+  for (int i = a; i <= b; ++i) if (is_palindrome_num(i)) ++count;
+  std::cout << count << "\n";
+}
