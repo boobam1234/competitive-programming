0c6ba51cd38d4458d928d7722736d51683af7282
boobam <47242815+boobam1279@users.noreply.github.com>
2023-01-23 20:54:00 +0900

    feat: add abc081 b_shift_only.cc

---
+++ atcoder/abc/081/b_shift_only.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+bool can_operate(std::vector<int> &numbers) {
+  for (int i = 0; i < numbers.size(); ++i) {
+    if (numbers[i] % 2 != 0) return false;
+  }
+  return true;
+}
+
+void operate(std::vector<int> &numbers) {
+  for (int i = 0; i < numbers.size(); ++i) numbers[i] /= 2;
+}
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  int n;
+  std::cin >> n;
+  std::vector<int> a(n);
+  for (int i = 0; i < n; ++i) std::cin >> a[i];
+
+  int count = 0;
+  while (can_operate(a)) {
+    operate(a);
+    ++count;
+  }
+
+  std::cout << count << "\n";
+}
